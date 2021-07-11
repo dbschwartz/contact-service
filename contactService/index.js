e298031e0de9e3e7c3c5fce545cc8456da979704
David Schwartz <dbschwartz2@gmail.com>
2021-07-11 13:35:53 -0600

    initial attempt

--- contactService/index.js
+++ contactService/index.js
@@ -5,11 +5,52 @@
 // Feel free to add files as necessary
 
 export default class {
-    constructor(updates, service) {
+  constructor(updates, service) {
+    this.arr = [];
+    updates.on("add", async (id) => {
+      const newContact = await service.getById(id);
+      this.arr.push(newContact);
+    });
+    updates.on("change", async (id, field, value) => {
+      const contacttoUpdate = this.arr.find(
+        (contact) => contact.id === id
+      );
+      contacttoUpdate[field] = value;
+    });
+    updates.on("remove", async (id) => {
+      this.arr.splice(this.arr.findIndex(contact => contact.id === id), 1);
+    });
+  }
 
-    }
-
-    search(query) {
-        return [];
-    }
+  search(query) {
+    const results = this.arr.filter(
+      ({
+        firstName,
+        lastName,
+        primaryPhoneNumber,
+        secondaryPhoneNumber,
+        primaryEmail,
+        secondaryEmail,
+        addressLine1,
+        addressLine2,
+        addressLine3,
+        city,
+        state,
+      }) =>
+        firstName.includes(query) ||
+        lastName.includes(query) ||
+        primaryPhoneNumber.includes(query) ||
+        secondaryPhoneNumber.includes(query) ||
+        primaryEmail.includes(query) ||
+        secondaryEmail.includes(query) ||
+        addressLine1.includes(query) ||
+        addressLine2.includes(query) ||
+        addressLine3.includes(query) ||
+        city.includes(query) ||
+        state.includes(query) ||
+        id.includes(query)
+    );
+    //onsole.log(results);
+    return results;
+  }
 }
